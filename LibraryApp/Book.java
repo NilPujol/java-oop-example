b04f23743f7e21021defa142f5b7dcb4f5f930e8
Nil Pujol <nilpujolporta@gmail.com>
2022-04-20 13:00:32 +0200

    Primera fase de refactoritzar

---
+++ LibraryApp/Book.java
@@ -0,0 +1,18 @@
+package LibraryApp;
+
+class Book {
+  private String id;
+  private String title;
+  public String getTitle() {
+    return title;
+  }
+  public String getId() {
+    return id;
+  }
+  public void setId(String id) {
+    this.id = id;
+  }
+  public void setTitle(String title) {
+    this.title = title;
+  }
+}
---
+++ LibraryApp/Reader.java
@@ -0,0 +1,33 @@
+package LibraryApp;
+
+import java.util.ArrayList;
+
+class Reader {
+  private String id;
+  private String name;
+  public ArrayList<Book> borrowedBooks = new ArrayList<Book>();
+
+  public void receiveBook(Book book) {
+    this.borrowedBooks.add(book);
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void giveBook(Book book) {
+    this.borrowedBooks.remove(book);
+  }
+}
